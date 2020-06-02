1956f771ec66f770eee8b5a1c91874ae2a8336d2
Tui Tpk <tanamet.tpk@gmail.com>
2020-06-02 10:39:15 +0700

    add model and controller

--- server/src/controllers/reviews.js
+++ server/src/controllers/reviews.js
@@ -1,9 +1,14 @@
+const Models = require('../models')
+const Reviews = Models.reviews
 
 module.exports = {
 
     getById: async (req, res) => {
 
-        res.success([])
+        let id = req.params.id
+        let review = await Reviews.findById({_id: id})
+
+        res.success(review)
 
     },
 
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -2,4 +2,6 @@
 
 module.exports = {
 
+    reviews: require('./reviews'),
+
 }
