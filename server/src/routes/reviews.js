82a7a98adf4c6f3000b01b93f847317e123117f5
Tui Tpk <tanamet.tpk@gmail.com>
2020-06-02 12:31:13 +0700

    remove unused middlewares

--- server/src/routes/reviews.js
+++ server/src/routes/reviews.js
@@ -4,12 +4,10 @@ module.exports = (Models) => {
     const router = require( "express" ).Router()
     const Reviews = require('../controllers/reviews')(Models)
 
-    router
-        .get("/", Reviews.getByKeyword)
+    router.get("/", Reviews.getByKeyword)
 
-    router
-        .get("/:id", Reviews.getById)
-        .put(Reviews.editReview)
+    router.get("/:id", Reviews.getById)
+    router.put("/:id", Reviews.editReview)
 
     return router
 
